adbab1b640163ac30c44d04d4b8966fabd06169e
leeso <leesub0168@gmail.com>
2023-06-08 22:28:47 +0900

    MemberRequestDto / MemberResponseDto 생성

---
+++ src/main/java/com/org/houserent/controller/MemberRequestDto.java
@@ -0,0 +1,28 @@
+package com.org.houserent.controller;
+
+import com.org.houserent.service.MemberDto;
+import lombok.Getter;
+
+@Getter
+public class MemberRequestDto {
+    private String user_id;
+    private String password;
+    private String name;
+    private String email;
+
+    public MemberRequestDto(String user_id, String password) {
+        this.user_id = user_id;
+        this.password = password;
+    }
+
+    public MemberRequestDto(String user_id, String password, String name, String email) {
+        this.user_id = user_id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public MemberDto toMemberDto() {
+        return new MemberDto(getUser_id(), getName(), getPassword(), getEmail());
+    }
+}
---
+++ src/main/java/com/org/houserent/controller/MemberResponseDto.java
@@ -0,0 +1,11 @@
+package com.org.houserent.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberResponseDto {
+    private String name;
+    private String email;
+}
