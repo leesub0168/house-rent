327529fce085e3ce2d01b5e7b22aabb74dfba472
leeso <leesub0168@gmail.com>
2023-06-08 22:27:11 +0900

    MemberDto를 서비스 계층용 dto로 변경

--- src/main/java/com/org/houserent/service/MemberDto.java
+++ src/main/java/com/org/houserent/service/MemberDto.java
@@ -1,9 +1,11 @@
 package com.org.houserent.service;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.org.houserent.domain.Member;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class MemberDto {
     private String user_id;
     private String name;
@@ -17,10 +19,16 @@ public class MemberDto {
         this.email = email;
     }
 
+    public MemberDto(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
     public MemberDto(Member member) {
         this.name = member.getName();
         this.email = member.getEmail();
         this.user_id = member.getUser_id();
         this.password = member.getPassword();
     }
+
 }
