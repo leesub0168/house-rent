cc29e5a4ed336835ae5c0470122ed85fad6a09f3
leeso <leesub0168@gmail.com>
2023-06-14 23:04:52 +0900

    생성자 builder 추가 , 필드 정리

--- src/main/java/com/org/houserent/domain/BookMark.java
+++ src/main/java/com/org/houserent/domain/BookMark.java
@@ -1,6 +1,7 @@
 package com.org.houserent.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 
 @Entity
 public class BookMark {
@@ -15,4 +16,13 @@ public class BookMark {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "house_id")
     private House house;
+
+    public BookMark() {
+    }
+
+    @Builder
+    public BookMark(Member member, House house) {
+        this.member = member;
+        this.house = house;
+    }
 }
