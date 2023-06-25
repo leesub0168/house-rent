60bb6a654fd1d94fa5bae3a447e6ee75f4fe3161
leeso <leesub0168@gmail.com>
2023-06-25 22:09:47 +0900

    실거래가 api dto 기능추가

--- src/main/java/com/org/houserent/util/publicApi/dto/HouseSaleApiDataDto.java
+++ src/main/java/com/org/houserent/util/publicApi/dto/HouseSaleApiDataDto.java
@@ -3,9 +3,13 @@ package com.org.houserent.util.publicApi.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.org.houserent.service.dto.HouseDto;
+import com.org.houserent.service.dto.HouseSaleContractDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -25,4 +29,26 @@ public class HouseSaleApiDataDto extends SeoulResponseBaseDto {
     private String HOUSE_TYPE;
     private String REQ_GBN;
     private String RDEALER_LAWDNM;
+
+    public HouseSaleContractDto toHouseSaleContractDto(HouseDto houseDto) {
+        return HouseSaleContractDto.builder()
+                .sale_contract_date(dealYmdToLocalDateTime())
+                .floor(getFLOOR())
+                .sale_price(Integer.parseInt(getOBJ_AMT()))
+                .building_area(getBLDG_AREA())
+                .site_area(getTOT_AREA())
+                .right_gbn(getRIGHT_GBN())
+                .declare_type(getREQ_GBN())
+                .declare_estate_agent_address(getRDEALER_LAWDNM())
+                .houseDto(houseDto)
+                .build();
+    }
+
+    public LocalDateTime dealYmdToLocalDateTime() {
+        int year = Integer.parseInt(getDEAL_YMD().substring(0, 4));
+        int month = Integer.parseInt(getDEAL_YMD().substring(4, 6));
+        int day = Integer.parseInt(getDEAL_YMD().substring(6, 8));
+
+        return LocalDateTime.of(year, month, day, 0, 0);
+    }
 }
