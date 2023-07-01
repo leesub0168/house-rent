24cfe7ef16a639e4dd6acd47a4412bdff66c3fd8
leeso <leesub0168@gmail.com>
2023-07-01 17:22:03 +0900

    HouseRepository -> Spring Data Jpa 적용후 테스트 & 코드 정리

--- src/main/java/com/org/houserent/repository/HouseRepository.java
+++ src/main/java/com/org/houserent/repository/HouseRepository.java
@@ -1,47 +1,17 @@
 package com.org.houserent.repository;
 
 import com.org.houserent.domain.House;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.NoResultException;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
-public class HouseRepository {
+import java.util.Optional;
 
-    @PersistenceContext
-    private EntityManager em;
+public interface HouseRepository extends JpaRepository<House, Long> {
+    @Query("select h from House h where concat(h.road_name,' ', h.building_main_num,'-', h.building_sub_num) like concat('%',:searchAddress, '%')")
+    Optional<House> findHouseByRoadAddress(@Param("searchAddress") String searchAddress);
 
-    public void saveHouse(House house) {
-        em.persist(house);
-    }
-
-    public House findHouseById(Long id) {
-        return em.find(House.class, id);
-    }
-
-    public House findHouseByRoadAddress(String searchAddress) {
-        try {
-            return em.createQuery(
-                            "select h from House h " +
-                                    "where concat(h.road_name,' ', h.building_main_num,'-', h.building_sub_num) like concat('%',:searchAddress, '%')", House.class)
-                    .setParameter("searchAddress", searchAddress)
-                    .getSingleResult();
-        } catch (NoResultException ne) {
-            return null;
-        }
-    }
-
-    public House findHouseByLandAddress(String searchAddress) {
-        try {
-            return em.createQuery(
-                            "select h from House h " +
-                                    "where concat(h.dong,' ', h.land_main_num,'-', h.land_sub_num) like concat('%',:searchAddress, '%')", House.class)
-                    .setParameter("searchAddress", searchAddress)
-                    .getSingleResult();
-        } catch (NoResultException ne) {
-            return null;
-        }
-    }
+    @Query("select h from House h where concat(h.dong,' ', h.land_main_num,'-', h.land_sub_num) like concat('%',:searchAddress, '%')")
+    Optional<House> findHouseByLandAddress(@Param("searchAddress") String searchAddress);
 
 }
