cf6c55a888c026d6f230e5f446a661b8cd842ee7
leeso <leesub0168@gmail.com>
2023-07-07 23:02:14 +0900

    HouseRentContractRepository - Spring Data JPA 적용 HouseRentContractService - 코드 리팩토링 예외추가

--- src/main/java/com/org/houserent/repository/HouseRentContractRepository.java
+++ src/main/java/com/org/houserent/repository/HouseRentContractRepository.java
@@ -2,30 +2,10 @@ package com.org.houserent.repository;
 
 import com.org.houserent.domain.House;
 import com.org.houserent.domain.HouseRentContract;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-@Repository
-public class HouseRentContractRepository {
-    @PersistenceContext
-    private EntityManager em;
-
-    public void saveHouseRentContract(HouseRentContract houseRentContract) {
-        em.persist(houseRentContract);
-    }
-
-    public HouseRentContract findHouseRentContractById(Long id) {
-        return em.find(HouseRentContract.class, id);
-    }
-
-    public List<HouseRentContract> findHouseRentContractByHouse(Long houseId) {
-        return em.createQuery(
-                        "select hr from HouseRentContract hr join hr.house h " +
-                                "where h.id = :houseId", HouseRentContract.class)
-                .setParameter("houseId", houseId)
-                .getResultList();
-    }
+public interface HouseRentContractRepository extends JpaRepository<HouseRentContract, Long> {
+    List<HouseRentContract> findHouseRentContractsByHouse(House house);
 }
