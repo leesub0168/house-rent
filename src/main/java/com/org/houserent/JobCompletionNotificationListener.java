772e4d2586daeb97c10c13335676b3f5558cbdd7
leeso <leesub0168@gmail.com>
2023-07-13 03:25:03 +0900

    배치 + 스케쥴러 데이터 입력 초기 세팅

---
+++ src/main/java/com/org/houserent/JobCompletionNotificationListener.java
@@ -0,0 +1,30 @@
+package com.org.houserent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JobCompletionNotificationListener implements JobExecutionListener {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        log.info("#### JOB STARTED! ##########################");
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            log.info("!!! JOB FINISHED! Time to verify the results");
+
+        }
+    }
+}
