53958eedeba51a92ecb1fe4ad61e54b364e77f8a
leeso <leesub0168@gmail.com>
2023-07-31 23:31:44 +0900

    변수명으로 인한 파라미터 매핑 오류로 이름 변경 + SearchService 초기 테스트

--- src/main/java/com/org/houserent/service/SearchService.java
+++ src/main/java/com/org/houserent/service/SearchService.java
@@ -1,9 +1,15 @@
 package com.org.houserent.service;
 
+import com.org.houserent.controller.dto.request.SearchRequestDto;
+import com.org.houserent.controller.dto.response.SearchHouseResponseDto;
 import com.org.houserent.controller.dto.response.SearchResponseDto;
+import com.org.houserent.exception.NonExistHouseException;
+import com.org.houserent.service.dto.HouseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -11,8 +17,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class SearchService {
     private final MemberService memberService;
     private final HouseService houseService;
-//
-//    public SearchResponseDto searchAddress(String userId, String keyword) {
-//
-//    }
+
+    public SearchResponseDto searchAddress(SearchRequestDto searchRequestDto) {
+        Optional<HouseDto> houseDto;
+        if(searchRequestDto.isRoadAddressYn()) {
+            houseDto = houseService.findHouseByRoadAddress(searchRequestDto.getSearchAddress());
+        } else {
+            houseDto = houseService.findHouseByLandAddress(searchRequestDto.getSearchAddress());
+        }
+
+        houseDto.orElseThrow(() -> new NonExistHouseException("주소 정보를 찾을 수 없습니다."));
+
+        SearchResponseDto searchResponseDto = new SearchResponseDto();
+        searchResponseDto.setHouse(new SearchHouseResponseDto(houseDto.get()));
+
+        return searchResponseDto;
+    }
 }
