3eb6123d9d99c19d24864ce3c787ab2e2c483552
leeso <leesub0168@gmail.com>
2023-08-29 21:58:55 +0900

    배포스크립트 오타 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ec2-user/app/step1"
-JAR_FILE="$PROJECT_ROOT/프로젝트 이름.jar"
 REPOSITORY=/home/ec2-user/app/step1
 PROJECT_NAME=house-rent
 
@@ -18,7 +17,7 @@ cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_NAME 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar JAR_NAME > $APP_LOG 2> $ERROR_LOG &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f JAR_NAME)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
